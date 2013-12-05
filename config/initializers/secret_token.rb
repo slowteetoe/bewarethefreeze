48a17835750d56951a2abebf497c185d94074122
Steven Lotito <steven.lotito@alumni.pitt.edu>
2013-12-05 13:55:20 -0800

    App skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bewarethefreeze::Application.config.secret_key_base = '8ff444cba574f639d050cb3ef36221d0553f349fdaf7122d51485ee4071c8e1c2d52052429025bc90a0c8e9a4ba59fa6b7bd52cf74e05925c2180c6a3014d722'
