1d14dacd95efb10378357125f40940d72c368096
Steven Lotito <steven.lotito@alumni.pitt.edu>
2013-12-05 17:47:29 -0800

    Don't break if we haven't run 'data:import' yet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
   	loc = Location.find_by_zip "89031"
-  	@forecast = ForecastIO.forecast(loc.lat, loc.long)
+  	@forecast = ForecastIO.forecast(loc.lat, loc.long) unless loc.nil?
   end
 
 end
